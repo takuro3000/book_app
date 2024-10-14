4c0df1ffd70eacc345886f5ffed5a80530ce59b5
takuro <talangxiaotian6@gmail.com>
2024-10-14 12:50:28 +0900

    難易度の平均の表示

--- app/models/book.rb
+++ app/models/book.rb
@@ -1,4 +1,12 @@
 class Book < ApplicationRecord
   has_one_attached :image
   has_many :posts, dependent: :destroy
+
+  def avg_difficulty
+    unless self.posts.empty?
+      posts.average(:difficulty).round(1).to_f
+    else
+      0.0
+    end
+  end
 end
