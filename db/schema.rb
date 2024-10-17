e0c8d2a8fc3c57d1608d26f58453834dd7e16b5b
takuro <talangxiaotian6@gmail.com>
2024-10-17 15:19:11 +0900

    本の詳細なカラムの整備

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_10_13_191733) do
+ActiveRecord::Schema[7.1].define(version: 2024_10_17_060014) do
   create_table "active_storage_attachments", force: :cascade do |t|
     t.string "name", null: false
     t.string "record_type", null: false
@@ -53,16 +53,15 @@ ActiveRecord::Schema[7.1].define(version: 2024_10_13_191733) do
 
   create_table "books", force: :cascade do |t|
     t.string "title"
-    t.integer "difficulty"
     t.text "description"
     t.integer "volume"
     t.date "published_day"
     t.string "language"
     t.string "company"
     t.text "link"
-    t.integer "post_count"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "author"
   end
 
   create_table "likes", force: :cascade do |t|
