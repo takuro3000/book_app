4eac79d2a160f396c8b4961b0e6efa5821c76a28
takuro <talangxiaotian6@gmail.com>
2024-11-24 13:43:05 +0900

    ターボの修正

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,5 +1,6 @@
 // Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
 //= require jquery
 //= require rails-ujs
-import "@hotwired/turbo-rails"
+import { Turbo } from "@hotwired/turbo-rails"
+Turbo.session.drive = false
 import "controllers"
