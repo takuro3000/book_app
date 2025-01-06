9b90527f395f37b6760f17c4385e8662385d5ea8
takuro <talangxiaotian6@gmail.com>
2025-01-06 19:59:07 +0900

    ホームページの本の表示数の制限

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,7 +1,7 @@
 class PagesController < ApplicationController
   def home
-    @books_post = Book.order_by_post_count
-    @books_new = Book.latest
+    @books_post = Book.order_by_post_count.limit(12)
+    @books_new = Book.latest.limit(12)
   end
 
   def about
