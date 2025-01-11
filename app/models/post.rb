478ace748c9e374e4768e000a11f5b2b1119fc54
takuro <talangxiaotian6@gmail.com>
2025-01-11 23:54:35 +0900

    postモデルのcontentカラムのバリデーション設定

--- app/models/post.rb
+++ app/models/post.rb
@@ -8,4 +8,6 @@ class Post < ApplicationRecord
   validates :difficulty,
             presence: true,
             numericality: { only_integer: true, greater_than: 0, less_than: 6 }
+
+  validates :content, length: { maximum: 500 }
 end
