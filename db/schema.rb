157e43667af6eef49663a08aa102f92acff17a6b
takuro <talangxiaotian6@gmail.com>
2025-01-11 13:57:23 +0900

    adminクラスのemailカラムの削除・関連するファイルの修正

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2025_01_10_135632) do
+ActiveRecord::Schema[7.1].define(version: 2025_01_11_043406) do
   create_table "active_storage_attachments", force: :cascade do |t|
     t.string "name", null: false
     t.string "record_type", null: false
@@ -40,14 +40,12 @@ ActiveRecord::Schema[7.1].define(version: 2025_01_10_135632) do
   end
 
   create_table "admins", force: :cascade do |t|
-    t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
     t.string "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.index ["email"], name: "index_admins_on_email", unique: true
     t.index ["reset_password_token"], name: "index_admins_on_reset_password_token", unique: true
   end
 
