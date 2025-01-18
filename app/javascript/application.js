df23870c4abab973c073d1b4171fb8959572cfeb
takuro <talangxiaotian6@gmail.com>
2025-01-18 21:52:14 +0900

    importmapでの設定のため不要な行の削除

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,5 +1,3 @@
 // Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
-//= require jquery
-//= require rails-ujs
 import "@hotwired/turbo-rails"
 import "controllers"
