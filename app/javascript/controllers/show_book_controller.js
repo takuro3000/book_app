a2e3f54fe84330ea1b875438af7247be416d2587
takuro <talangxiaotian6@gmail.com>
2025-02-05 05:25:33 +0900

    本詳細ページのデザインの修正

--- app/javascript/controllers/show_book_controller.js
+++ app/javascript/controllers/show_book_controller.js
@@ -6,38 +6,10 @@ export default class extends Controller {
     // ここに書いた処理は、コントローラがアタッチされたタイミングで毎回実行される
     console.log("show_book_controller connected!!")
 
-    // 以下、もともと show_book.js に書いていた処理をペーストする
-    const userPosts = document.querySelectorAll('.user-post-content'); // 全てのユーザー投稿を取得
-    userPosts.forEach(postElement => {
-        const maxAllowedHeight = 96; // 約4行分の高さ
-        if (postElement.scrollHeight > maxAllowedHeight) {
-            const postElementId = postElement.id.split('_')[2];
-            const showMoreBtn = document.getElementById('showMoreBtn_' + postElementId);
-            if (showMoreBtn) {
-                showMoreBtn.style.display = "inline"; // 「もっと見る」ボタンを表示
-                postElement.style.maxHeight = maxAllowedHeight + "px";
-                postElement.style.overflow = "hidden";
-
-                // 「もっと見る」ボタンのクリックイベント
-                showMoreBtn.addEventListener("click", () => {
-                    if (postElement.classList.contains('expanded')) {
-                        postElement.classList.remove('expanded');
-                        postElement.style.maxHeight = maxAllowedHeight + "px";
-                        showMoreBtn.textContent = "続きを読む";
-                    } else {
-                        postElement.classList.add('expanded');
-                        postElement.style.maxHeight = "none";
-                        showMoreBtn.textContent = "閉じる";
-                    }
-                });
-            }
-        }
-    });
-
     // ユーザー以外の投稿の高さを確認
     const posts = document.querySelectorAll('.post-content');
     posts.forEach(post => {
-        const maxHeight = 96; // 約4行分の高さ
+        const maxHeight = 100; // 約4行分の高さ
         if (post.scrollHeight > maxHeight) {
             const postId = post.id.split('_')[1];
             const readMoreBtn = document.getElementById('readMoreBtn_' + postId);
