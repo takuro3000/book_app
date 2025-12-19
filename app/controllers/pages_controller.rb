d75fb176f9f0a42a155e0c5e8e6dd40176c46cf8
takuro3000 <talangxiaotian6@gmail.com>
2025-12-19 14:53:53 +0900

    homeアクションの変数名を修正

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,10 +1,10 @@
 class PagesController < ApplicationController
   def home
-    @books_popular = fetch_books_by_difficulty(0, 5)
-    @books_beginner = fetch_books_by_difficulty(1, 3)
-    @books_intermediate = fetch_books_by_difficulty(2, 4)
-    @books_advanced = fetch_books_by_difficulty(3, 5)
-    @books_new = Book.latest.top_ranked
+    @most_reviewed_books = fetch_books_by_difficulty(0, 5)
+    @beginner_books = fetch_books_by_difficulty(1, 3)
+    @intermediate_books = fetch_books_by_difficulty(2, 4)
+    @advanced_books = fetch_books_by_difficulty(3, 5)
+    @latest_books = Book.latest.top_ranked
   end
 
   def about
