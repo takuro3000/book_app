b1db4b2dbde747a560dc93069d7ed0f9a981faf5
takuro3000 <talangxiaotian6@gmail.com>
2025-12-23 19:51:16 +0900

    styleではなくclassListで記述

--- app/javascript/controllers/read_more_controller.js
+++ app/javascript/controllers/read_more_controller.js
@@ -5,26 +5,26 @@ export default class extends Controller {
   static values = { maxHeight: { type: Number, default: 100 } }
 
   connect() {
-    // コンテンツの高さがmaxHeightを超えている場合、「続きを見る」ボタンを表示
     if (this.contentTarget.scrollHeight > this.maxHeightValue) {
-      this.contentTarget.style.maxHeight = this.maxHeightValue + "px"
-      this.contentTarget.style.overflow = "hidden"
-      this.expandButtonTarget.style.display = "inline"
+      this.collapseContent()
     }
   }
 
+  collapseContent() {
+    this.contentTarget.classList.add("is-collapsed")
+    this.expandButtonTarget.classList.add("is-displayed")
+  }
+
   expand() {
-    this.contentTarget.style.maxHeight = "none"
-    this.contentTarget.style.overflow = "visible"
-    this.expandButtonTarget.style.display = "none"
-    this.collapseButtonTarget.style.display = "inline"
+    this.contentTarget.classList.remove("is-collapsed")
+    this.expandButtonTarget.classList.remove("is-displayed")
+    this.collapseButtonTarget.classList.add("is-displayed")
   }
 
   collapse() {
-    this.contentTarget.style.maxHeight = this.maxHeightValue + "px"
-    this.contentTarget.style.overflow = "hidden"
-    this.expandButtonTarget.style.display = "inline"
-    this.collapseButtonTarget.style.display = "none"
+    this.contentTarget.classList.add("is-collapsed")
+    this.expandButtonTarget.classList.add("is-displayed")
+    this.collapseButtonTarget.classList.remove("is-displayed")
   }
 }
 
