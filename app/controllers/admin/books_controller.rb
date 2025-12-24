8238ed1797356d9113045df1243d3239892cd2f2
takuro3000 <talangxiaotian6@gmail.com>
2025-12-24 12:33:49 +0900

    searchアクションの中身をServiceクラスに切り出し

--- app/controllers/admin/books_controller.rb
+++ app/controllers/admin/books_controller.rb
@@ -1,5 +1,4 @@
 class Admin::BooksController < ApplicationController
-  require 'net/http'
   before_action :authenticate_admin!
   before_action :set_book, only: %i[show edit update destroy]
 
@@ -38,24 +37,9 @@ class Admin::BooksController < ApplicationController
   end
 
   def search
-    # Google Books APIでタイトルをもとに検索
-    title = params[:title]
-    book_info = fetch_book_info(title)
-
-    if book_info[:error]
-      @book = Book.new
-      @error_message = "本が見つかりませんでした。"
-    else
-      @book = Book.new(
-        title: book_info[:title],
-        author: book_info[:authors],
-        description: book_info[:description],
-        published_day: book_info[:published_date],
-        volume: book_info[:volume],
-        language: book_info[:language] == 'ja' ? '日本語' : book_info[:language],
-        company: book_info[:company]
-      )
-    end
+    result = GoogleBooksService.new(params[:title]).call
+    @book = result[:book]
+    @error_message = result[:error_message]
 
     respond_to do |format|
       format.turbo_stream
@@ -73,34 +57,5 @@ class Admin::BooksController < ApplicationController
       :title, :description, :image, :volume, :published_day, :language,
       :company, :link ,:author ,:category
     )
-  end
-
-  def fetch_book_info(title)
-    query = URI.encode_www_form(q: title)
-    uri = URI("https://www.googleapis.com/books/v1/volumes?#{query}")
-
-    http = Net::HTTP.new(uri.host, uri.port)
-    http.use_ssl = true
-    # 開発環境ではSSL証明書の検証をスキップ（macOSのCRL問題回避）
-    http.verify_mode = Rails.env.development? ? OpenSSL::SSL::VERIFY_NONE : OpenSSL::SSL::VERIFY_PEER
-
-    request = Net::HTTP::Get.new(uri)
-    response = http.request(request)
-    books = JSON.parse(response.body)
-
-    if books['items']
-      book_info = books['items'].first['volumeInfo']
-      {
-        title: book_info['title'],
-        authors: book_info['authors']&.join(', '),
-        description: book_info['description'],
-        published_date: book_info['publishedDate'],
-        volume: book_info['pageCount'],
-        language: book_info['language'],
-        company: book_info['publisher']
-      }
-    else
-      { error: 'No book found' }
-    end
   end
 end
