6933c2e907a09ccc085ddf8ffbe34eeb4ed4b918
takuro3000 <talangxiaotian6@gmail.com>
2025-12-24 10:49:48 +0900

    jsをturbo streamに修正

--- app/controllers/admin/books_controller.rb
+++ app/controllers/admin/books_controller.rb
@@ -40,20 +40,10 @@ class Admin::BooksController < ApplicationController
   def search
     # Google Books APIでタイトルをもとに検索
     title = params[:title]
-    book_info = fetch_book_info(title)
+    @book_info = fetch_book_info(title)
 
-    if book_info[:error]
-      render json: { error: '本が見つかりませんでした。' }
-    else
-      render json: {
-        title: book_info[:title],
-        authors: book_info[:authors],
-        description: book_info[:description] || '',
-        published_date: book_info[:published_date],
-        volume: book_info[:volume],
-        language: book_info[:language],
-        company: book_info[:company]
-      }
+    respond_to do |format|
+      format.turbo_stream
     end
   end
 
@@ -72,10 +62,16 @@ class Admin::BooksController < ApplicationController
 
   def fetch_book_info(title)
     query = URI.encode_www_form(q: title)
-    url = URI("https://www.googleapis.com/books/v1/volumes?#{query}")
+    uri = URI("https://www.googleapis.com/books/v1/volumes?#{query}")
 
-    response = Net::HTTP.get(url)
-    books = JSON.parse(response)
+    http = Net::HTTP.new(uri.host, uri.port)
+    http.use_ssl = true
+    # 開発環境ではSSL証明書の検証をスキップ（macOSのCRL問題回避）
+    http.verify_mode = Rails.env.development? ? OpenSSL::SSL::VERIFY_NONE : OpenSSL::SSL::VERIFY_PEER
+
+    request = Net::HTTP::Get.new(uri)
+    response = http.request(request)
+    books = JSON.parse(response.body)
 
     if books['items']
       book_info = books['items'].first['volumeInfo']
